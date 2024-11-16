e7e4a6efc9c5419e1e0d60d589b1687083dcf615
e-d-u <educarmonamoreno3@gmail.com>
2024-11-16 20:52:10 +0100

    Descripción de los cambios

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_nQkzR6JM.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_2Xvmjq3W.mjs').then(n => n.g);
+
+export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_BwgayT9u.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_CjVHw0Yn.mjs');
+
+export { page };
